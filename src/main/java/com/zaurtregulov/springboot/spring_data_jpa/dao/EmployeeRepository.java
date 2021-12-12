a2e200f2718135a1bb0a83bb326f4d4a2d4fb731
RomanKovalenko61 <astek14@mail.ru>
2021-12-12 17:11:19 +0300

    Course: Spring for beginners

---
+++ src/main/java/com/zaurtregulov/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.springboot.spring_data_jpa.dao;
+
+
+import com.zaurtregulov.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByName(String name);
+}
